245de572476a94e0c66bb97d347c71e1c8c0fbfa
Gudao Luo <gudao.luo@gmail.com>
2020-12-20 17:03:48 +0100

    basic_104 Hello Single Project!

---
+++ change_app_id.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# This file is used for another project.
+
+# Usage
+# ./change_app_id <NEU_APP_IP>
+
+sed -i "s/basic_104/basic_${1}/g" build.gradle
+N1=`echo $1 | cut -c1-1`
+N2=`echo $1 | cut -c2-2`
+N3=`echo $1 | cut -c3-3`
+sed -i 's/'1.0.4'/'$N1.$N2.$N3'/g' build.gradle
